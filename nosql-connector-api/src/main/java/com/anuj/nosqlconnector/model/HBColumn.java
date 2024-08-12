ec84673a140438511aebe60db25a05130db053b8
anuj-mehra <mehra.anuj1712@gmail.com>
2024-08-12 13:14:29 +0530

    @anujmehra added model classes to handle hbase data

--- nosql-connector-api/src/main/java/com/anuj/nosqlconnector/model/HBColumn.java
+++ nosql-connector-api/src/main/java/com/anuj/nosqlconnector/model/HBColumn.java
@@ -1,5 +1,7 @@
 package com.anuj.nosqlconnector.model;
 
+import com.anuj.nosqlconnector.utils.Pair;
+
 import java.io.Serializable;
 
 public class HBColumn implements Serializable {
@@ -8,5 +10,34 @@ public class HBColumn implements Serializable {
 
     private String columnFamily;
 
-    private
+    /**
+     * Pair for ColumnName-ColumnValue
+     */
+    private Pair<String , ? extends Serializable> columnNameValue;
+
+    public HBColumn(final String columnFamily, final Pair<String, ? extends Serializable> columnNameValue){
+        this.columnFamily = columnFamily;
+        this.columnNameValue = columnNameValue;
+    }
+
+    public HBColumn(final String columnFamily, final String columnName, final byte[] columnValue){
+        this.columnFamily = columnFamily;
+        this.columnNameValue = new Pair<String, byte[]>(columnName, columnValue);
+    }
+
+    public String getColumnFamily() {
+        return columnFamily;
+    }
+
+    public void setColumnFamily(String columnFamily) {
+        this.columnFamily = columnFamily;
+    }
+
+    public Pair<String, ? extends Serializable> getColumnNameValue() {
+        return columnNameValue;
+    }
+
+    public void setColumnNameValue(Pair<String, ? extends Serializable> columnNameValue) {
+        this.columnNameValue = columnNameValue;
+    }
 }
--- nosql-connector-api/src/main/java/com/anuj/nosqlconnector/model/HBTableRowMapping.java
+++ nosql-connector-api/src/main/java/com/anuj/nosqlconnector/model/HBTableRowMapping.java
@@ -1,4 +1,72 @@
 package com.anuj.nosqlconnector.model;
 
-public class HBTableRowMapping {
+import org.apache.commons.collections.CollectionUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * <p>
+ *     Model class to store the values/records that needs to be stored in the HBase table mapped by the <code>tableName</code>.
+ * </p>
+ * @param <T>
+ */
+public class HBTableRowMapping<T extends Serializable> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Parameter to hold the name of the hbase table.
+     */
+    private String tableName;
+
+    /**
+     * Parameter to hold the rowkey.
+     */
+    private T rowKey;
+
+    /**
+     * Collection of column-famiy, column-name and column-value.
+     */
+    private Collection<HBColumn> columns;
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public T getRowKey() {
+        return rowKey;
+    }
+
+    public void setRowKey(T rowKey) {
+        this.rowKey = rowKey;
+    }
+
+    public Collection<HBColumn> getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Collection<HBColumn> columns) {
+        this.columns = columns;
+    }
+
+    public void addColumnToCollection(final HBColumn column){
+        columns.add(column);
+    }
+
+    public void addColumn(final HBColumn column){
+        if(CollectionUtils.isEmpty(this.columns)){
+            this.columns = new ArrayList<>();
+        }
+        columns.add(column);
+    }
+
+    public void createCollection(){
+        this.columns = new ArrayList<>();
+    }
 }
